015d5b43c9a68ddcf7b656df3bd0e57726e9ed31
Mo0dy - Felix Mühlenberend <felix.muehlenberend@gmail.com>
2017-05-09 02:37:04 +0200

    added basic group dynamics as temporary fix

--- TDS-Test/TDS-Test/AwesomeFace.cpp
+++ TDS-Test/TDS-Test/AwesomeFace.cpp
@@ -13,7 +13,7 @@ AwesomeFace::AwesomeFace(glm::vec2 position) : Enemy(position)
 	state = MOVING;
 	airFricCoeff = -1;
 
-	damage = 50;
+	damage = 800;
 	attacking = true;
 	attackSpeed = 0.5;
 }
@@ -40,15 +40,21 @@ GLboolean AwesomeFace::updateE(GLfloat dt) {
 	// Pathfinding to player
 	addForce(glm::normalize(Game::Players[0]->pos - pos) * movForce);
 
+	for(DynE *e : Game::dynEntities) {
+		if (e != this) {
+			addForce(glm::normalize(pos - e->pos) * 350.0f /  glm::pow(glm::distance(pos, e->pos), 4));
+		}
+	}
+
 	glm::vec2 dV = dt * force / mass;
 
 	// safeguard for wiggeling close to 0v
-	if (vel.x * (vel.x + dV.x) <= 0 && vel.y * (vel.y + dV.y) <= 0) {
-		vel = glm::vec2(0, 0);
-	}
-	else {
+	//if (vel.x * (vel.x + dV.x) <= 0 && vel.y * (vel.y + dV.y) <= 0) {
+	//	vel = glm::vec2(0, 0);
+	//}
+	//else {
 		vel += dV;
-	}
+	//}
 
 	color = glm::vec3(1.0f, health / 100.0f, 1.0f);
 
