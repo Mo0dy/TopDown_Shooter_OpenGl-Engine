c3358e7058c4ee75b964155ad5da8e6900f5f158
Thomas Knippenberg <thomasknip27@gmail.com>
2017-05-14 11:32:25 +0200

    Commented E_Drone.cpp

--- TDS-Test/TDS-Test/E_Drone.cpp
+++ TDS-Test/TDS-Test/E_Drone.cpp
@@ -1,9 +1,11 @@
 #include "E_Drone.h"
 
+//Load texture
 void E_Drone::load_E_Drone() {
 	ResourceManager::LoadAnimation("Textures\\A_Drone", ".png", 31, 1.5, GL_TRUE, "E_Drone_A", HBOX_AUTOFIT);
 }
 
+//Constructor
 E_Drone::E_Drone(glm::vec2 position) : Enemy(position)
 {
 	maxHealth = 70;
@@ -19,6 +21,7 @@ E_Drone::E_Drone(glm::vec2 position) : Enemy(position)
 
 	turnSpeed = 10;
 
+	//Set animation
 	Animations["Flying"] = Animation("E_Drone_A", 1.0, GL_TRUE);
 	Animations["Flying"].animationTime = 1;
 	Animations["Flying"].startAnimation();
@@ -29,13 +32,17 @@ E_Drone::E_Drone(glm::vec2 position) : Enemy(position)
 	swarmPot = 2.05;
 }
 
+//Destructor
 E_Drone::~E_Drone()
 {
 }
 
+//Update
 GLboolean E_Drone::updateE(GLfloat dt) {
+	//Don't update if it's dead
 	if (!death) 
 	{
+		//Check for death
 		if (health <= 0) {
 			death = GL_TRUE;
 			return GL_FALSE;
@@ -43,10 +50,12 @@ GLboolean E_Drone::updateE(GLfloat dt) {
 		lastAttack += dt;
 		updateAni();
 
+		//Check if attacking is possible
 		if (!attacking && lastAttack > attackSpeed) {
 			attacking = GL_TRUE;
 		}
 
+		//Find the closest player to attack
 		Player* gPlayer = Game::Players[0];
 
 		for (int i = 0; i < Game::Players.size(); i++) {
@@ -56,6 +65,8 @@ GLboolean E_Drone::updateE(GLfloat dt) {
 		}
 
 		glm::vec2 movDir = glm::normalize(gPlayer->pos - pos) * movForce;
+
+		//Drones all hate eachother and try to stay away from other enemies
 		for (Enemy *e : Game::Enemies) {
 			if (glm::distance(pos, e->pos) > 0.4) {
 				movDir += glm::normalize(pos - e->pos) * swarmFactor / glm::pow(glm::distance(pos, e->pos), 2);
