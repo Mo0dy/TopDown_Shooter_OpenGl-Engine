4b56b5e262bc20d4c9a8ae0257062b0e24769730
Mo0dy - Felix Mühlenberend <felix.muehlenberend@gmail.com>
2017-05-16 13:30:09 +0200

    Rewrote HitBox and Entity

--- TDS-Test/TDS-Test/HitObject.h
+++ TDS-Test/TDS-Test/HitObject.h
@@ -9,6 +9,11 @@ public:
 	HitObject(glm::vec2 pos, glm::vec2 size, GLfloat angle);
 	~HitObject();
 
+	// Getters and setters
+	glm::vec2 GetPos();
+	glm::vec2 GetSize();
+	GLfloat GetAngle();
+
 protected:
 	glm::vec2 pos; // center of the hitbox proportionate to the size of the texture
 	glm::vec2 size; // Size of the rectangle from lower left corner as proportion to the width and height;
